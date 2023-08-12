5839ad8e96ee4795046987f427f996538eb3e5ac
cristianofernando <110285936+cristianofernando@users.noreply.github.com>
2023-08-12 19:20:40 +1000

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -23,7 +23,7 @@ console.log(actual_answer);
 
 question_number = "<h4>" + number1 + " X " + number2 + "</h4>";
 input_box = "<br>Answer : <input type='text' id='input_check_box'>";
-check_button = "<br><br><button class='btn btn-info' onclick=check()'>Check</button>";
+check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
 row = question_number + input_box + check_button ;
 document.getElementById("output").innerHTML = row;
 document.getElementById("number1").value = "";
@@ -37,17 +37,18 @@ answer_turn = "player2";
 
 function check()
 {
+    console.log("check call")
     get_answer = document.getElementById("input_check_box").value; 
     if(get_answer == actual_answer)
     {
         if(answer_turn == "player1")
         {
-            player1_score = player1_score =1;
+            player1_score = player1_score +1;
             document.getElementById("player1_score").innerHTML = player1_score;
         }
         else 
         {
-            player2_score + player2_score +1;
+            player2_score = player2_score +1;
             document.getElementById("player2_score").innerHTML = player2_score;
         }
     }
